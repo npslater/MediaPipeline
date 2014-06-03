d31e68fa2c257251cb52446ce0dc8e869401597d
Nate Slater <nateslate@gmail.com>
2014-06-02 20:28:18 -0700

    Changed guard config

--- spec/media_file_spec.rb
+++ spec/media_file_spec.rb
@@ -7,6 +7,10 @@ describe MediaFile do
     expect(mf).not_to be_nil
   end
 
+  it 'should do nothing' do
+
+  end
+
   it 'should return a hash when tag_data is called' do
     mf = MediaFile.new('./media_files/file1.m4a')
     expect(mf.tag_data).to be_an_instance_of(Hash)
