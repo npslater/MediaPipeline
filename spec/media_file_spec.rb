622598c966dfb0e1a669de51752524f6578a52bc
Nate Slater <nateslate@gmail.com>
2014-06-02 19:58:04 -0700

    Added libs and tests

---
+++ spec/media_file_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+describe MediaFile do
+
+  it 'should have a getter for the file property' do
+    mf = MediaFile.new('/this/is/the/path')
+    expect(mf).not_to be_nil
+  end
+
+  it 'should return a hash when tag_data is called' do
+    mf = MediaFile.new('./media_files/file1.m4a')
+    expect(mf.tag_data).to be_an_instance_of(Hash)
+  end
+end
