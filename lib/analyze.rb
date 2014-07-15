763e4cb6bb3b2bd4691caed908aa8c3a42acc752
Nate Slater <nateslate@gmail.com>
2014-07-14 20:11:13 -0700

    Added debugging statements.

--- lib/analyze.rb
+++ lib/analyze.rb
@@ -85,6 +85,8 @@ module MediaPipeline
         v.each do | media_file |
           analysis[:hash_key_len].push(media_file.file.length)
           tag_length = 0
+          #TODO: debugging remove later
+          puts "reading tag from #{media_file.file}"
           media_file.tag_data(false).keys.each do | tag |
             tag_length = tag_length + media_file.tag_data[tag].to_s.length
           end
